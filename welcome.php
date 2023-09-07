2dc7df569577793d1546669d65c4530f6b24330b
sakshamgulati <sakshamgulati123@gmail.com>
2023-09-07 19:15:08 -0400

    finetuned and changed welcome.php

--- welcome.php
+++ welcome.php
@@ -1,8 +1,27 @@
-<html>
-<body>
+<?php
+// welcome.php
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-</body>
-</html>
+    // Insert data into a MySQL database (example)
+    $conn = new mysqli("localhost", "username", "password", "database_name");
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql = "INSERT INTO feedback (email, subject, message) VALUES ('$email', '$subject', '$message')";
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+}
+?>
